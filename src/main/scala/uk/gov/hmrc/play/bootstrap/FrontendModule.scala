0a168cdfd60751c91a239d672cce69f304192d32
Michael Wolfendale <michael.wolfendale@gmail.com>
2017-09-04 11:38:46 +0100

    Added ApplicationCrypto instance to FE Module

--- src/main/scala/uk/gov/hmrc/play/bootstrap/FrontendModule.scala
+++ src/main/scala/uk/gov/hmrc/play/bootstrap/FrontendModule.scala
@@ -19,6 +19,7 @@ package uk.gov.hmrc.play.bootstrap
 import play.api.http.HttpFilters
 import play.api.inject.Binding
 import play.api.{Configuration, Environment}
+import uk.gov.hmrc.crypto.{ApplicationCrypto, ApplicationCryptoDI}
 import uk.gov.hmrc.play.bootstrap.filters.AuditFilter
 import uk.gov.hmrc.play.bootstrap.filters.frontend._
 import uk.gov.hmrc.play.bootstrap.filters.frontend.crypto.{CookieCryptoFilter, DefaultCookieCryptoFilter, SessionCookieCrypto, SessionCookieCryptoProvider}
@@ -31,6 +32,7 @@ class FrontendModule extends BootstrapModule {
       bind[HttpFilters].to[FrontendFilters],
       bind[AuditFilter].to[FrontendAuditFilter],
       bind[SessionCookieCrypto].toProvider[SessionCookieCryptoProvider],
+      bind[ApplicationCrypto].to[ApplicationCryptoDI],
       bind[CookieCryptoFilter].to[DefaultCookieCryptoFilter],
       bind[FrontendAuditFilter].to[DefaultFrontendAuditFilter],
       bind[DeviceIdFilter].to[DefaultDeviceIdFilter],
