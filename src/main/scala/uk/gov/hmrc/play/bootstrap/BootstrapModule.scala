b7e26d452f8939fe6c822b4946e40aa191cd26e8
Michael Wolfendale <michael.wolfendale@gmail.com>
2017-09-10 09:05:51 +0100

    Added class to add information to MDC context on startup

--- src/main/scala/uk/gov/hmrc/play/bootstrap/BootstrapModule.scala
+++ src/main/scala/uk/gov/hmrc/play/bootstrap/BootstrapModule.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.play.bootstrap
 
 import play.api.inject.{Binding, Module}
 import play.api.{Configuration, Environment}
-import uk.gov.hmrc.play.bootstrap.config.ControllerConfigs
+import uk.gov.hmrc.play.bootstrap.config.{ControllerConfigs, MDCInstance, MDCLoggingSetup}
 import uk.gov.hmrc.play.bootstrap.filters.{CacheControlConfig, DefaultLoggingFilter, LoggingFilter}
 
 abstract class BootstrapModule extends Module {
@@ -26,6 +26,8 @@ abstract class BootstrapModule extends Module {
   override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = Seq(
     bind[CacheControlConfig].toInstance(CacheControlConfig.fromConfig(configuration)),
     bind[ControllerConfigs].toInstance(ControllerConfigs.fromConfig(configuration)),
-    bind[LoggingFilter].to[DefaultLoggingFilter]
+    bind[LoggingFilter].to[DefaultLoggingFilter],
+    bind[MDCInstance].toSelf.eagerly,
+    bind[MDCLoggingSetup].toSelf.eagerly
   )
 }
---
+++ src/test/scala/uk/gov/hmrc/play/bootstrap/config/MDCLoggingSetupSpec.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.play.bootstrap.config
+
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterEach, MustMatchers, WordSpec}
+import play.api.inject.guice.GuiceApplicationBuilder
+import org.mockito.Mockito._
+import org.mockito.Matchers.{eq => eqTo, _}
+
+class MDCLoggingSetupSpec extends WordSpec with MustMatchers with MockitoSugar with BeforeAndAfterEach {
+
+  val mdc: MDCInstance = mock[MDCInstance]
+
+  val builder: GuiceApplicationBuilder = {
+
+    import play.api.inject._
+
+    new GuiceApplicationBuilder()
+      .bindings(
+        bind[MDCInstance].toInstance(mdc),
+        bind[MDCLoggingSetup].toSelf.eagerly
+      )
+      .configure(
+        "appName" -> "Test"
+      )
+  }
+
+  override def beforeEach(): Unit = {
+    reset(mdc)
+    super.beforeEach()
+  }
+
+  "new" must {
+
+    "add the appName to the MDC context" in {
+      builder.build()
+      verify(mdc).put("appName", "Test")
+    }
+
+    "not add the dateformat when it's not set" in {
+      builder.build()
+      verify(mdc, never).put(eqTo("json.logger.dateformat"), any())
+    }
+
+    "add the dateformat when it's set" in {
+      builder.configure("logger.json.dateformat" -> "foobar").build()
+      verify(mdc).put("logger.json.dateformat", "foobar")
+    }
+  }
+}
