7254300a1246327c6249b2de47b90c42a71b0bee
Jakob Grunig <jakob.grunig@digital.hmrc.gov.uk>
2017-11-15 11:21:46 +0000

    PLATOPS-1131 - Deprecate play-graphite into bootstrap-play-25

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -47,20 +47,20 @@ object Dependencies {
     "uk.gov.hmrc" %% "crypto" % "4.4.0",
     "uk.gov.hmrc" %% "http-verbs" % "7.2.0",
     "uk.gov.hmrc" %% "http-verbs-play-25" % "0.9.0",
-    "uk.gov.hmrc" %% "play-auditing" % "3.2.0",
+    "uk.gov.hmrc" %% "play-auditing" % "3.2.1",
     "uk.gov.hmrc" %% "auth-client" % "2.3.0",
     "uk.gov.hmrc" %% "play-health" % "2.1.0",
-    "uk.gov.hmrc" %% "play-graphite" % "4.1.0",
     "uk.gov.hmrc" %% "play-config" % "5.0.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
     "com.typesafe.play" %% "play" % "2.5.12",
+    "io.dropwizard.metrics" % "metrics-graphite" % "3.2.5",
     "de.threedimensions" %% "metrics-play" % "2.5.13",
     "ch.qos.logback" % "logback-core" % "1.1.7"
   )
 
   val test = Seq(
     "com.typesafe.play" %% "play-test" % PlayVersion.current % "test",
-    "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+    "org.scalacheck" % "scalacheck_2.11" % "1.12.5" % "test",
     "org.mockito" % "mockito-all" % "1.9.5" % "test",
     "org.pegdown" % "pegdown" % "1.5.0" % "test",
     "com.github.tomakehurst" % "wiremock" % "2.7.1" % "test",
---
+++ src/main/scala/uk/gov/hmrc/play/bootstrap/graphite/GraphiteMetricsModule.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.play.bootstrap.graphite
+
+import com.codahale.metrics.MetricFilter
+import com.codahale.metrics.graphite.{Graphite, GraphiteReporter}
+import com.kenshoo.play.metrics._
+import play.api.inject.{Binding, Module}
+import play.api.{Configuration, Environment}
+import uk.gov.hmrc.play.config.RunMode
+
+class GraphiteMetricsModule extends Module {
+
+  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
+    val defaultBindings: Seq[Binding[_]] = Seq(
+      // Note: `MetricFilter` rather than `MetricsFilter`
+      bind[MetricFilter].toInstance(MetricFilter.ALL).eagerly
+    )
+
+    val kenshoBindings : Seq[Binding[_]] =
+      if (kenshoMetricsEnabled(configuration)) {
+        Seq(
+          bind[MetricsFilter].to[MetricsFilterImpl].eagerly,
+          bind[Metrics].to[MetricsImpl].eagerly)
+      } else {
+        Seq(
+          bind[MetricsFilter].to[DisabledMetricsFilter].eagerly,
+          bind[Metrics].to[DisabledMetrics].eagerly)
+      }
+
+    val graphiteConfiguration = extractGraphiteConfiguration(environment, configuration)
+
+    val graphiteBindings: Seq[Binding[_]] =
+      if (kenshoMetricsEnabled(configuration) && graphitePublisherEnabled(graphiteConfiguration)) {
+        Seq(
+          bind[GraphiteProviderConfig].toInstance(GraphiteProviderConfig.fromConfig(graphiteConfiguration)),
+          bind[GraphiteReporterProviderConfig].toInstance(GraphiteReporterProviderConfig.fromConfig(configuration, graphiteConfiguration)),
+          bind[Graphite].toProvider[GraphiteProvider],
+          bind[GraphiteReporter].toProvider[GraphiteReporterProvider],
+          bind[GraphiteReporting].to[EnabledGraphiteReporting].eagerly
+        )
+      } else {
+        Seq(
+          bind[GraphiteReporting].to[DisabledGraphiteReporting].eagerly
+        )
+      }
+
+    defaultBindings ++ graphiteBindings ++ kenshoBindings
+  }
+
+  private def kenshoMetricsEnabled(rootConfiguration: Configuration) =
+    rootConfiguration.getBoolean("metrics.enabled").getOrElse(false)
+
+  private def graphitePublisherEnabled(graphiteConfiguration: Configuration) =
+    graphiteConfiguration.getBoolean("enabled").getOrElse(false)
+
+  private def extractGraphiteConfiguration(environment: Environment, configuration: Configuration): Configuration = {
+    val env = RunMode(environment.mode, configuration).env
+    configuration.getConfig(s"$env.microservice.metrics.graphite")
+      .orElse(configuration.getConfig("microservice.metrics.graphite"))
+      .getOrElse(Configuration())
+  }
+}
