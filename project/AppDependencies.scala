df0c786caaee920df695ce02df8759d8ed0532c3
Konrad Wudkowski <wudkowski@gmail.com>
2018-06-28 11:29:50 +0100

    PLATOPS-1387: Upgraded to latest version of play-health

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ object AppDependencies {
     "io.dropwizard.metrics" % "metrics-graphite"     % "3.2.5",
     "com.kenshoo"           %% "metrics-play"        % "2.6.6_0.6.2",
     "ch.qos.logback"        % "logback-core"         % "1.1.7",
-    "uk.gov.hmrc"           %% "play-health"         % "3.0.0"
+    "uk.gov.hmrc"           %% "play-health"         % "3.2.0"
   )
 
   val test = Seq(
