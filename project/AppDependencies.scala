253aef6eb3a0ddac4ee8bcc072e4ea9322a3360b
Konrad Wudkowski <wudkowski@gmail.com>
2019-01-15 10:58:48 +0000

    Upgrade play-auditing to latest version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,7 +16,7 @@ object AppDependencies {
     "uk.gov.hmrc"           %% "crypto"              % "5.1.0",
     "uk.gov.hmrc"           %% "http-verbs"          % "9.0.0-play-26",
     "uk.gov.hmrc"           %% "logback-json-logger" % "4.0.0",
-    "uk.gov.hmrc"           %% "play-auditing"       % "3.13.0-play-26",
+    "uk.gov.hmrc"           %% "play-auditing"       % "3.15.0-play-26",
     "uk.gov.hmrc"           %% "play-health"         % "3.8.0-play-26",
     "uk.gov.hmrc"           %% "time"                % "3.2.0",
     // force dependencies due to security flaws found in jackson-databind < 2.9.x using XRay
