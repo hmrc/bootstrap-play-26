29ec1094f8abb0680f6e7606a3de1682524b3139
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2019-11-18 17:39:29 +0000

    BDOG-487 Downgrade akka libraries after last play bump, for compatibility with reactivemongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,11 @@ import sbt._
 
 object AppDependencies {
 
-  private val playVersion = "2.6.24"
+  private val playVersion     = "2.6.24"
+  // 2.5.23 is last compatible version with reactivemongo
+  // https://github.com/akka/akka/blob/master/akka-actor/src/main/mima-filters/2.5.23.backwards.excludes
+  private val akkaVersion     = "2.5.23"
+  private val akkaHttpVersion = "10.0.15"
 
   val compile: Seq[ModuleID] = Seq(
     "ch.qos.logback"        % "logback-core"         % "1.1.7",
@@ -24,7 +28,13 @@ object AppDependencies {
     "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.9.7",
     "com.fasterxml.jackson.core"     % "jackson-annotations"     % "2.9.7",
     "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8"   % "2.9.7",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.7"
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.7",
+    // downgrade akka dependency for compatibility with reactivemongo (since play 2.6.23)
+    "com.typesafe.akka" %% "akka-stream"    % akkaVersion     force(),
+    "com.typesafe.akka" %% "akka-protobuf"  % akkaVersion     force(),
+    "com.typesafe.akka" %% "akka-slf4j"     % akkaVersion     force(),
+    "com.typesafe.akka" %% "akka-actor"     % akkaVersion     force(),
+    "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion force()
   )
 
   val test: Seq[ModuleID] = Seq(
